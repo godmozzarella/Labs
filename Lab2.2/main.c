3cfa82b45356a961bb6601e1149a070dfc3c197b
KIrill <143953592+godmozzarella@users.noreply.github.com>
2023-09-05 19:34:11 +0300

    2.2

--- Lab2.2/main.c
+++ Lab2.2/main.c
@@ -1,14 +1,39 @@
-//
-//  main.c
-//  Lab2.2
-//
-//  Created by Кирилл Пустовойт on 05.09.2023.
-//
-
 #include <stdio.h>
+/*
+
+//№1
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    printf("3) %s\t", "Это текст");//1
+    printf("%s\n\n", "Это текст");//3
+    printf("5) %.5f\n\n", 12.234657);//5
+    printf("7) %d / %d = %d\n", 7,5,7/5);//7
+    printf("7) %d * %d = %d\n\n", 7,5,7/5);//7
+    printf ("9) %f / %f = %f\n",5., 2., 5./2);//9 вычисление в формате float
+    printf ("9) %d / %d = %d\n",5, 2, 5/2);//9 формат d не работает с числами с "." (форматом float), ответ округлен
+    printf ("9) %g / %g = %g\n",5., 2., 5./2);//9 точный ответ
+    printf ("9) %e / %e = %e\n\n",5., 2., 5./2);//9 другое
 }
+
+//№2
+
+int main(int argc, const char * argv[]) {
+    short  n = 2, k = 22, x;
+    printf("1) Сейчас %d часа %d минуты 00 секунд\n", n,k);
+    printf("2) Идет %d минута суток\n",143);
+    printf("3) До полуночи остался %d час и %d минут\n", 21,38);
+    printf("4) С 8.00 прошло %d секунд\n",(18*60*+22)*60);
+    printf("5) Текущий час  = %g суток  и текущая минута = %g часа\n", 2./24,22./60);
+}
+
+//№3
+
+int main(int argc, const char * argv[]) {
+    short e=3;
+    float n=3;
+
+    printf("       %d\n     %d\n ---------\n  %0+8.3f\n ",e,333, n/333);
+    
+}
+*/
+

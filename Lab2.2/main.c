5643c8fdbdf274d7d7564dfdd7a8f6147a2b0d7b
KIrill <143953592+godmozzarella@users.noreply.github.com>
2023-09-05 19:23:27 +0300

    Initial Commit

---
+++ Lab2.2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Lab2.2
+//
+//  Created by Кирилл Пустовойт on 05.09.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

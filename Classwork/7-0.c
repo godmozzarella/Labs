1dd8657f2be6c369a2898634e89ee2dd01a1393a
Kirill Zhoski <143953592+godmozzarella@users.noreply.github.com>
2023-10-19 21:33:52 +0300

    Update and rename main.c to 7-0.c

--- Classwork/7-0.c
+++ Classwork/7-0.c
@@ -4,45 +4,45 @@ int main(void)
 {
 //1
 
-//char С;
-//printf("Введите цифру или букву: ");
-//scanf("%c", &С);//считывание с консоли
-//switch (С)
-//{
-//    case '1':
-//        printf("Это цифра\n");
-//        break;
-//    case '2':
-//        printf("Это цифра\n");
-//        break;
-//    case '3':
-//        printf("Это цифра\n");
-//        break;
-//    case '4':
-//        printf("Это цифра\n");
-//        break;
-//    case '5':
-//        printf("Это цифра\n");
-//        break;
-//    case '6':
-//        printf("Это цифра\n");
-//        break;
-//    case '7':
-//        printf("Это цифра\n");
-//        break;
-//    case '8':
-//        printf("Это цифра\n");
-//        break;
-//    case '9':
-//        printf("Это цифра\n");
-//        break;
-//    case '0':
-//        printf("Это цифра\n");
-//        break;
-//    default:
-//        printf("Это буква\n");
-//}
-//return 0;
+char С;
+printf("Введите цифру или букву: ");
+scanf("%c", &С);//считывание с консоли
+switch (С)
+{
+   case '1':
+       printf("Это цифра\n");
+       break;
+   case '2':
+       printf("Это цифра\n");
+       break;
+   case '3':
+       printf("Это цифра\n");
+       break;
+   case '4':
+       printf("Это цифра\n");
+       break;
+   case '5':
+       printf("Это цифра\n");
+       break;
+   case '6':
+       printf("Это цифра\n");
+       break;
+   case '7':
+       printf("Это цифра\n");
+       break;
+   case '8':
+       printf("Это цифра\n");
+       break;
+   case '9':
+       printf("Это цифра\n");
+       break;
+   case '0':
+       printf("Это цифра\n");
+       break;
+   default:
+       printf("Это буква\n");
+}
+return 0;
 
 //2
 

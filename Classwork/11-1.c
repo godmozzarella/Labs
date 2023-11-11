7d6ca213f4828fc8850b03d4cf178dc806999987
Kirill Zhoski <143953592+godmozzarella@users.noreply.github.com>
2023-11-11 14:46:20 +0300

    Update 11-1.c

--- Classwork/11-1.c
+++ Classwork/11-1.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <math.h>
 #define N 10
-////1
-//int main(void)
-//{
-//    int A[N],B[N];
-//    char c = ' ';
-//    for(int i=0; i<10; ++i)
-//    {
-//        printf("a[%d] = ", i+1);
-//        int temp;
-//        scanf("%d", &temp);
-//        A[i]=temp;
-//
-//    }
-//    for(int i=0; i<10; ++i)
-//    {
-//        B[i] = A[i] + A[9];
-//        printf("|%4c%5d%6c|%4c%5d%6c|%4c%5d%6c|\n",c,i,c,c,A[i],c,c,B[i],c);
-//    }
-//   printf("%.3f\n",(float)(A[0]+A[1]+A[2]+A[3]+A[4]+A[5]+A[6]+A[7]+A[8]+A[9])/N);
-//}
+//1
+int main(void)
+{
+   int A[N],B[N];
+   char c = ' ';
+   for(int i=0; i<10; ++i)
+   {
+       printf("a[%d] = ", i+1);
+       int temp;
+       scanf("%d", &temp);
+       A[i]=temp;
+
+   }
+   for(int i=0; i<10; ++i)
+   {
+       B[i] = A[i] + A[9];
+       printf("|%4c%5d%6c|%4c%5d%6c|%4c%5d%6c|\n",c,i,c,c,A[i],c,c,B[i],c);
+   }
+  printf("%.3f\n",(float)(A[0]+A[1]+A[2]+A[3]+A[4]+A[5]+A[6]+A[7]+A[8]+A[9])/N);
+}
 
 //2
 
